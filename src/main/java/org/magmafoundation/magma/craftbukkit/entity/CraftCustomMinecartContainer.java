3172445167b9dec5995807a084b21319dea38c9d
JustRed23 <manuel.bormans@gmail.com>
2022-07-04 22:16:24 +0000

    Fix/lootr

---
+++ src/main/java/org/magmafoundation/magma/craftbukkit/entity/CraftCustomMinecartContainer.java
@@ -0,0 +1,48 @@
+/*
+ * Magma Server
+ * Copyright (C) 2019-2022.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.magmafoundation.magma.craftbukkit.entity;
+
+import net.minecraft.world.entity.vehicle.AbstractMinecart;
+import net.minecraft.world.entity.vehicle.AbstractMinecartContainer;
+import org.bukkit.craftbukkit.v1_18_R2.CraftServer;
+import org.bukkit.craftbukkit.v1_18_R2.entity.CraftMinecartContainer;
+import org.bukkit.entity.EntityType;
+import org.jetbrains.annotations.NotNull;
+
+public class CraftCustomMinecartContainer extends CraftMinecartContainer {
+
+    private final EntityType entityType;
+
+    public CraftCustomMinecartContainer(CraftServer server, AbstractMinecart entity) {
+        super(server, entity);
+        this.entityType = entity.getBukkitEntity().getType();
+    }
+
+    @Override
+    public @NotNull EntityType getType() {
+        return entityType;
+    }
+
+    @Override
+    public String toString() {
+        return "CraftCustomMinecartConatiner{" +
+                "entityType=" + entityType +
+                '}';
+    }
+}
