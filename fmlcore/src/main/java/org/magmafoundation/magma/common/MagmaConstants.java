5bd75c72c4254d18c28172f464e2579ebf49ee59
JustRed23 <manuel.bormans@gmail.com>
2022-07-15 21:18:40 +0200

    Update craftbukkit version

--- fmlcore/src/main/java/org/magmafoundation/magma/common/MagmaConstants.java
+++ fmlcore/src/main/java/org/magmafoundation/magma/common/MagmaConstants.java
@@ -8,7 +8,7 @@ public class MagmaConstants {
     public static final String NAME = "Magma";
     public static final String BRAND = "MagmaFoundation";
     public static final String VERSION = !Objects.equals(fullVersion, "dev-env") ? fullVersion.split("-")[0] + "-" + fullVersion.split("-")[2] : "dev-env";
-    public static final String BUKKIT_VERSION = "v1_16_R3";
+    public static final String BUKKIT_VERSION = "v1_18_R2";
     public static final String FORGE_VERSION_FULL = fullVersion;
     public static final String FORGE_VERSION = fullVersion.substring(0, fullVersion.lastIndexOf("-"));
     public static final String NMS_PREFIX = "net/minecraft/server/";
