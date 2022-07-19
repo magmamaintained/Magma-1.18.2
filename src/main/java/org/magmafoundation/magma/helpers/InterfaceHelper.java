9f03961d027e2163024d99b6dcfb0e457671931b
JustRed23 <manuel.bormans@gmail.com>
2022-07-19 20:07:30 +0200

    fixes
    Fix AbstractMethodError on mods implementing Level
    
    Merge branch 'fix/stuck-in-the-end' into '1.18.x'
    
    Fix getTypeKey returning null making people in the end get stuck
    
    See merge request magmafoundation/Magma-1-18-x!36
    
    Fix ClassCastException with mods implementing a Level class
    
    Merge branch 'fix/cce-level' into '1.18.x'
    
    Fix ClassCastException with mods implementing a Level class
    
    See merge request magmafoundation/Magma-1-18-x!37
    
    Fix Bukkit throwing an exception when a Forge non-default category was used
    
    Fix glares crashing the server when they tried to eat a glowberry
    
    Fix IndexOutOfBoundsException when trying to enchant
    
    Merge branch 'fix/friends-and-foes' into '1.18.x'
    
    Fix/friends and foes
    
    See merge request magmafoundation/Magma-1-18-x!38
    
    Fix some items not firing useOn
    
    Add support for custom damage sources
    
    Remove check for empty item stacks, fixes Create depots
    
    Merge branch 'fix/create' into '1.18.x'
    
    Fix/create
    
    See merge request magmafoundation/Magma-1-18-x!40
    
    Merge branch 'fix/enchants-and-items' into '1.18.x'
    
    Fix/enchants and items
    
    See merge request magmafoundation/Magma-1-18-x!39

---
+++ src/main/java/org/magmafoundation/magma/helpers/InterfaceHelper.java
@@ -0,0 +1,7 @@
+package org.magmafoundation.magma.helpers;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public final class InterfaceHelper {
+    public static AtomicReference<net.minecraft.world.entity.Entity> tempNMSEntity = new AtomicReference<>(null);
+}
