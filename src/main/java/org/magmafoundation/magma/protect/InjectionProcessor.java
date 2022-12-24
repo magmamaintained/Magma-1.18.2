37a5eb53b604de2184e6b4f263498db0e5fa6c48
JustRed23 <manuel.bormans@gmail.com>
2022-12-24 22:41:53 +0100

    Revamp InjectionProcessor to be compatible with more failure messages

--- src/main/java/org/magmafoundation/magma/protect/InjectionProcessor.java
+++ src/main/java/org/magmafoundation/magma/protect/InjectionProcessor.java
@@ -14,13 +14,51 @@ public class InjectionProcessor {
         Throwable cause = ShortenedStackTrace.findCause(error);
         String message = cause.getMessage();
 
-        //example: Critical injection failure: LVT in ... has incompatible changes at opcode ... in callback MODHERE.mixins.json:MIXINCLASSHERE->@...
-        int index = message.indexOf("in callback") + "in callback".length() + 1;
+        //Special case, the function does not get references after the .json, so we have to grab it from the message
+        String annot = "annotation on";
+        if (message.contains(annot)) {
+            String function = getRangeFromChars(annot, " could not find", message);
+            if (function == null)
+                return null;
+
+            String json = getJsonFromString(message);
+            if (json == null)
+                return null;
+
+            return json + ":" + function;
+        }
+
+        return getJsonFromString(message);
+    }
+
+    private static @Nullable String getRangeFromChars(String startIndex, String endIndex, String message) {
+        int index = message.indexOf(startIndex) + startIndex.length() + 1; //begin after the specified start index + 1 to skip the space
         try {
-            return message.substring(index, message.indexOf("(", index));
+            return message.substring(index, message.indexOf(endIndex, index));
         } catch (StringIndexOutOfBoundsException e) {
             return null;
         }
+    }
+
+    private static @Nullable String getJsonFromString(String message) {
+        int index = message.indexOf(".mixins.json"); //should be required in all mixin errors
+        if (index == -1)
+            return null;
+
+        int start = message.substring(0, index).lastIndexOf(" "); //gets the space before the json name -> ex. "( )MODHERE.mixins.json "
+        if (start == -1)
+            return null;
+
+        int end = message.indexOf(" ", index); //gets the space after the json name -> ex. " MODHERE.mixins.json( )"
+        if (end == -1)
+            return null;
+
+        String raw = message.substring(start + 1, end); //gets the json name -> ex. "MODHERE.mixins.json"
+
+        if (raw.contains("(")) //if the mixin inject references a class, it will be cut off -> ex. "MODHERE.mixins.json:doThing(Lnet/minecraft/entity/Entity;)V"
+            raw = raw.substring(0, raw.indexOf("("));
+
+        return raw;
     }
 
     public static @Nullable String getModID(Throwable error) {
