090c2f8a8bceb3de01a57f51e2c8328f8ecbf3d1
JustRed23 <manuel.bormans@gmail.com>
2023-01-14 19:57:57 +0100

    Remove trailing slashes, should fix some libraries failing to download

--- magmalauncher/src/main/java/org/magmafoundation/magma/installer/MagmaInstaller.java
+++ magmalauncher/src/main/java/org/magmafoundation/magma/installer/MagmaInstaller.java
@@ -363,10 +363,10 @@ public class MagmaInstaller extends AbstractMagmaInstaller {
             manager.loadFromResource(new URL("jar:file:" + JarTool.getJarPath() + "!/data/magma_libraries.txt"));
 
             List<Repository> standardRepositories = new ArrayList<>();
-            standardRepositories.add(new StandardRepository("https://maven.minecraftforge.net/"));
-            standardRepositories.add(new StandardRepository("https://repo1.maven.org/maven2/"));
-            standardRepositories.add(new StandardRepository("https://raw.github.com/Hexeption/Magma-Repo/master/"));
-            standardRepositories.add(new StandardRepository("https://maven.izzel.io/releases/"));
+            standardRepositories.add(new StandardRepository("https://maven.minecraftforge.net"));
+            standardRepositories.add(new StandardRepository("https://repo1.maven.org/maven2"));
+            standardRepositories.add(new StandardRepository("https://raw.github.com/Hexeption/Magma-Repo/master"));
+            standardRepositories.add(new StandardRepository("https://maven.izzel.io/releases"));
 
             List<Dependency> dependencies = manager.getDependencies();
 
