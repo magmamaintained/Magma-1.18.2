83cebeff51d40dae39deb5c40fb892563b3c1c0a
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2023-05-26 12:16:34 +0200

    Implement Paper patch 0025 and Paper API 0010 Improved TPS statistics Also includes a small small portion of 0009
    https://github.com/PaperMC/Paper/blob/ver/1.18.2/patches/server/0025-Further-improve-server-tick-loop.patch
    https://github.com/PaperMC/Paper/blob/ver/1.18.2/patches/api/0010-Add-getTPS-method.patch

--- src/main/java/org/spigotmc/TicksPerSecondCommand.java
+++ src/main/java/org/spigotmc/TicksPerSecondCommand.java
@@ -24,20 +24,31 @@ public class TicksPerSecondCommand extends Command
             return true;
         }
 
-        StringBuilder sb = new StringBuilder( ChatColor.GOLD + "TPS from last 1m, 5m, 15m: " );
-        for ( double tps : MinecraftServer.getServer().recentTps )
-        {
-            sb.append( format( tps ) );
-            sb.append( ", " );
+        // Paper start - Further improve tick handling
+        double[] tps = org.bukkit.Bukkit.getTPS();
+        String[] tpsAvg = new String[tps.length];
+
+        for ( int i = 0; i < tps.length; i++) {
+            tpsAvg[i] = TicksPerSecondCommand.format_static( tps[i] ); //Magma - Renamed to format_static
         }
-        sender.sendMessage( sb.substring( 0, sb.length() - 2 ) );
-        sender.sendMessage(ChatColor.GOLD + "Current Memory Usage: " + ChatColor.GREEN + ((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024)) + "/" + (Runtime.getRuntime().totalMemory() / (1024 * 1024)) + " mb (Max: "
-                + (Runtime.getRuntime().maxMemory() / (1024 * 1024)) + " mb)");
+        sender.sendMessage(ChatColor.GOLD + "TPS from last 1m, 5m, 15m: " + org.apache.commons.lang.StringUtils.join(tpsAvg, ", "));
+        if (args.length > 0 && args[0].equals("mem") && sender.hasPermission("bukkit.command.tpsmemory")) {
+            sender.sendMessage(ChatColor.GOLD + "Current Memory Usage: " + ChatColor.GREEN + ((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024)) + "/" + (Runtime.getRuntime().totalMemory() / (1024 * 1024)) + " mb (Max: " + (Runtime.getRuntime().maxMemory() / (1024 * 1024)) + " mb)");
+            if (!hasShownMemoryWarning) {
+                sender.sendMessage(ChatColor.RED + "Warning: " + ChatColor.GOLD + " Memory usage on modern garbage collectors is not a stable value and it is perfectly normal to see it reach max. Please do not pay it much attention.");
+                hasShownMemoryWarning = true;
+            }
+        }
+        // Paper end
 
         return true;
     }
 
-    private String format(double tps)
+    private String format(double tps){
+        return TicksPerSecondCommand.format_static(tps);
+    }
+    private boolean hasShownMemoryWarning; // Paper
+    private static String format_static(double tps) // Paper - Made static //Magma - Renamed to format_static, to prevent possible conflicts.
     {
         return ( ( tps > 18.0 ) ? ChatColor.GREEN : ( tps > 16.0 ) ? ChatColor.YELLOW : ChatColor.RED ).toString()
                 + ( ( tps > 20.0 ) ? "*" : "" ) + Math.min( Math.round( tps * 100.0 ) / 100.0, 20.0 );
