64f4b5e217e03f1ebd6463bf5592814990541ff0
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2023-10-22 22:22:38 +0200

    fix incorrect elements in list (my issue) and duplicate elements (previous codebase issue)

--- src/main/java/org/magmafoundation/magma/craftbukkit/ItemMetaTransformer.java
+++ src/main/java/org/magmafoundation/magma/craftbukkit/ItemMetaTransformer.java
@@ -1,5 +1,6 @@
 package org.magmafoundation.magma.craftbukkit;
 
+import net.minecraft.util.SortedArraySet;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.forgespi.language.IModFileInfo;
@@ -15,7 +16,7 @@ import java.util.*;
 public final class ItemMetaTransformer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ItemMetaTransformer.class);
-    private static final List<String> NOT_TRANSFORMABLE = new ArrayList<>();
+    private static final SortedArraySet<String> NOT_TRANSFORMABLE = SortedArraySet.create();
 
     //Performance heavy, please only call when needed
     public static void loadPrefixes() {
@@ -37,7 +38,7 @@ public final class ItemMetaTransformer {
             int dots = 0;
             for (final ModFileScanData.ClassData clazzData : scanData.getClasses()) {
                 final String className = clazzData.clazz().getClassName();
-                if (className == null || className.isEmpty() || !IgnoreUtil.shouldCheck(className)) continue;
+                if (className == null || className.isEmpty() || numberDots(className) < 1 || !IgnoreUtil.shouldCheck(className)) continue;
                 if (prefix == null) {
                     prefix = className;
                     dots = numberDots(prefix);
@@ -48,14 +49,21 @@ public final class ItemMetaTransformer {
                 while (i < prefix.length() && i < className.length() && prefix.charAt(i) == className.charAt(i)) {
                     i++;
                 }
+                int less_dots = numberDots(prefix.substring(i));
+                //detect if we would over-shorten the path and exit before we do
+                if (dots - less_dots <= 1) break;
                 //`i` is fine to use as the index, because it is guaranteed to be smaller than the length of className and prefix
                 prefix = prefix.substring(0, i);
-                dots -= numberDots(prefix.substring(i)); //calculating the amount of dots left in the rest of the prefix.
+                dots -= less_dots; //calculating the amount of dots left in the rest of the prefix.
                 //this allows us to fast-fail out of the loop, since we only really care about if the common prefix has 2 or more dots
                 if (dots <= 1) break;
             }
             //we fast-fail out of the inner loop. now we can actually properly continue, since we are in the outer loop
             if (dots <= 1) continue; //ignore top level packages
+            if (!isClassTransformable(prefix)) {
+                LOGGER.debug("Skipping " + prefix + " because it is already not transformable");
+                continue;
+            }
             LOGGER.debug("Adding " + prefix + " to non-transformable list");
             NOT_TRANSFORMABLE.add(prefix);
         }
@@ -76,8 +84,11 @@ public final class ItemMetaTransformer {
         if (pkg.startsWith("net.minecraft.")) //ignore vanilla
             return true;
 
+        return isClassTransformable(pkg);
+    }
+    public static boolean isClassTransformable(final String name){
         for (String s : NOT_TRANSFORMABLE) {
-            if (pkg.startsWith(s))
+            if (name.startsWith(s))
                 return false;
         }
         return true;
